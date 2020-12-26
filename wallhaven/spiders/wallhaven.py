79568b9c3416c2fa41c1318f4f1161bac95fcadc
ZSX-DB <Sx82199609124535@outlook.com>
2020-12-27 01:26:09 +0800

    update

--- wallhaven/spiders/wallhaven.py
+++ wallhaven/spiders/wallhaven.py
@@ -1,24 +1,26 @@
-# import scrapy
-from scrapy import Spider
-
+from scrapy import Spider, Request
 
 class QuotesSpider(Spider):
     name = "wallhaven"
-    start_urls = [
-        'https://wallhaven.cc/toplist?page=1',
-        'https://wallhaven.cc/toplist?page=2'
-    ]
+    # start_urls = [
+    #     'https://wallhaven.cc/toplist?page=1',
+    #     'https://wallhaven.cc/toplist?page=2'
+    # ]
+
+    # 此种方法无法生成动态url
+    # start_url = []
+    # for i in range(1, 7):
+    #     start_url.append('https://wallhaven.cc/toplist?page=' + str(i))
+
+    # 动态生成初始URL
+    def start_requests(self):
+        for i in range(1, 5):
+            url = 'https://wallhaven.cc/toplist?page={p}'.format(p=i)
+            yield Request(url=url, callback=self.parse)
 
     def parse(self, response):
         for li in response.css('li'):
             yield {
-                'preview': li.css('a.preview').get(),
-                # 'href': li.css('figure a::attr(href)').extract()[0]
-                # response.css("div.abc a::attr(href)").extract()[0]
+                'href': li.css('a.preview::attr(href)').get(),
+                'img_src': li.css('img::attr(data-src)').get()
             }
-        # for quote in response.css('div.quote'):
-        #     yield {
-        #         'text': quote.css('span.text::text').get(),
-        #         'author': quote.css('small.author::text').get(),
-        #         'tags': quote.css('div.tags a.tag::text').getall(),
-        #     }
